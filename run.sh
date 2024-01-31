3af26afe13118159fe00f52566a4d85e2032ad92
HFooladi <fooladi.hosein@gmail.com>
2024-01-31 16:54:37 +0100

    changing run.sh to run on all the data and for all the models

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-dataset_names="HIV"
+dataset_category="TDC"
+dataset_names="CYP2C19"
+split_type="scaffold"
+filename="train.csv"
 declare -a model_names=("GCN" "GAT" "Weave" "MPNN" "AttentiveFP" "gin_supervised_contextpred" "gin_supervised_infomax" "gin_supervised_edgepred" "gin_supervised_masking" "NF")
 #declare -a model_names=("gin_supervised_contextpred" "gin_supervised_infomax" "gin_supervised_edgepred" "gin_supervised_masking")
 
 for model_name in "${model_names[@]}"
 do
-    python scripts/classification_train.py -c ./datasets/MoleculeNet/HIV/split/scaffold/train_balanced.csv -sc smiles -s random -mo $model_name -p classification_results/$dataset_names/$model_name -ne 20 
+    python scripts/classification_train.py -c ./datasets/$dataset_category/$dataset_names/split/$split_type/$filename -sc smiles -s random -mo $model_name -p classification_results/$dataset_category/$dataset_names/$model_name -ne 20 
 done
 
 
