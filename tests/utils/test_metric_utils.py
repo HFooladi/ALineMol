1f2db13083a9e6b244aff40a6894c3854f525819
HFooladi <fooladi.hosein@gmail.com>
2024-02-07 11:10:53 +0100

    add test folder for testing modules

---
+++ tests/utils/test_metric_utils.py
@@ -0,0 +1,31 @@
+
+
+
+def test_eval_roc_auc():
+    pass
+
+
+def test_eval_pr_auc():
+    pass
+
+
+def test_eval_acc():
+    pass
+
+
+def test_rescale():
+    pass
+
+
+
+def test_compute_linear_fit():
+    pass
+
+
+
+def test_Meter():
+    pass    
+
+
+
+
---
+++ tests/utils/test_split_utils.py
@@ -0,0 +1,13 @@
+
+
+
+def test_split_hypers():
+    pass
+
+
+def test_split_molecules_train_test():
+    pass
+
+
+def test_split_molecules_train_val_test():
+    pass
