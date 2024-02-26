18063edd98ce7281861e01ee207b37e80ccfc4d4
HFooladi <fooladi.hosein@gmail.com>
2024-02-26 11:05:16 +0100

    add tests for splitters

---
+++ tests/splitters/test_splits.py
@@ -0,0 +1,22 @@
+import pytest
+from ALineMol.splitters.splits import *
+
+@pytest.fixture
+def molecules():
+    return ["C", "CC", "CCC", "CCCC", "CCCCC", "CCCCCC", "CCCCCCC", "CCCCCCCC", "CCCCCCCCC", "CCCCCCCCCC"]
+
+
+def Test_Scaffold():
+    pass
+
+def test_TargetProperty():
+    pass
+
+
+def test_MolecularWeight():
+    smiles = molecules()
+    mw = MolecularWeight(smiles)
+    mw._before_sample()
+    mw._sample()
+    mw._after_sample()
+
