1b4e04eb2a609a0cea611ee75012c6b06c193c6c
HFooladi <fooladi.hosein@gmail.com>
2024-06-11 18:13:28 +0200

    add configs for splitters

---
+++ alinemol/splitters/splitting_configures.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass
+class ScaffoldSplitConfig:
+    make_generic: bool = False
+
+@dataclass
+class MolecularWeightSplitConfig:
+    generalize_to_larger: bool = False
+
+@dataclass
+class KMeansSplitConfig:
+    n_clusters: int = 10
+    
