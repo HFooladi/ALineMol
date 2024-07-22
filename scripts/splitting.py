7ecde35c087355fe7097d717bf75918ef1da2ad0
HFooladi <fooladi.hosein@gmail.com>
2024-07-22 18:16:11 +0200

    change splitting to make sure that we can create balanced y split

--- scripts/splitting.py
+++ scripts/splitting.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 import pandas as pd
 import json
+from tqdm import tqdm
 
 
 from argparse import ArgumentParser
@@ -48,12 +49,12 @@ NAME_TO_MODEL_CONFIG: Dict[str, Any] = {
 def parse_args():
     parser = ArgumentParser("Splitting molecules into train and test sets")
     parser.add_argument(
-        "-f", "--file-path", type=str, required=True, help="Path to a .csv/.txt file of SMILES strings"
+        "-f", "--file_path", type=str, required=True, help="Path to a .csv/.txt file of SMILES strings"
     )
     parser.add_argument(
         "-sp", "--splitter", type=str, default="scaffold", help="The name of the splitter to use"
     )
-    parser.add_argument("-te", "--test-size", type=float, default=0.2, help="The size of the test set")
+    parser.add_argument("-te", "--test_size", type=float, default=0.2, help="The size of the test set")
     parser.add_argument("-nj", "--n_jobs", type=int, default=-1, help="Number of jobs to run in parallel")
     parser.add_argument(
         "-ns",
@@ -61,6 +62,9 @@ def parse_args():
         type=int,
         default=10,
         help="Number of splits to make (Reapeating the splitting process)",
+    )
+    parser.add_argument(
+        "-to", "--tolerance", type=float, default=0.1, help="Tolerance for the splitting process"
     )
     args = vars(parser.parse_args())
     return args
@@ -74,7 +78,9 @@ if __name__ == "__main__":
     test_size = args["test_size"]
     n_jobs = args["n_jobs"]
     n_splits = args["n_splits"]
+    tol = args["tolerance"]
 
+    internal_n_splits = 100  # Number of splits to make for test set
     config.update(args)
 
     if file_path.suffix == ".csv":
@@ -93,17 +99,25 @@ if __name__ == "__main__":
     smiles = df["smiles"].values
     hopts = NAME_TO_MODEL_CONFIG[splitter]
     if method in [KMeansSplit, MaxDissimilaritySplit, PerimeterSplit]:
-        splitter = method(n_splits=n_splits, test_size=test_size, random_state=42, **hopts)
+        splitter = method(n_splits=internal_n_splits, test_size=test_size, random_state=42, **hopts)
     elif method == MolecularWeightSplit:
-        splitter = method(smiles=smiles, n_splits=n_splits, test_size=test_size, random_state=42, **hopts)
+        splitter = method(
+            smiles=smiles, n_splits=internal_n_splits, test_size=test_size, random_state=42, **hopts
+        )
     else:
         splitter = method(
-            smiles=smiles, n_splits=n_splits, n_jobs=n_jobs, test_size=test_size, random_state=42, **hopts
+            smiles=smiles,
+            n_splits=internal_n_splits,
+            n_jobs=n_jobs,
+            test_size=test_size,
+            random_state=42,
+            **hopts,
         )
-    
+
     logger.info(splitter)
     logger.info(hopts)
 
+    """
     for i, (train_ind, test_ind) in enumerate(splitter.split(smiles)):
         train = df.iloc[train_ind]
         test = df.iloc[test_ind]
@@ -129,6 +143,44 @@ if __name__ == "__main__":
         config["test_actives_" + str(i)] = int(test["label"].sum())
         config["train_actives_percentage_" + str(i)] = train["label"].sum() / train["label"].shape[0]
         config["test_actives_percentage_" + str(i)] = test["label"].sum() / test["label"].shape[0]
+    """
+    i = 0
+    for train_ind, test_ind in tqdm(splitter.split(smiles)):
+        train = df.iloc[train_ind]
+        test = df.iloc[test_ind]
+        config["train_size_" + str(i)] = train.shape[0]
+        config["test_size_" + str(i)] = test.shape[0]
+        config["train_actives_" + str(i)] = int(train["label"].sum())
+        config["test_actives_" + str(i)] = int(test["label"].sum())
+        config["train_actives_percentage_" + str(i)] = train["label"].sum() / train["label"].shape[0]
+        config["test_actives_percentage_" + str(i)] = test["label"].sum() / test["label"].shape[0]
+
+        if (
+            config["train_actives_percentage_" + str(i)] - config["test_actives_percentage_" + str(i)] < tol
+            and config["train_actives_percentage_" + str(i)] - config["test_actives_percentage_" + str(i)]
+            > -tol
+        ):
+            i += 1
+        else:
+            continue
+        train.to_csv(increment_path(split_path / f"train_{i}.csv"), index=False)
+        test.to_csv(increment_path(split_path / f"test_{i}.csv"), index=False)
+
+        logger.info(
+            "percentage of actives in the train set: {}".format(
+                train["label"].sum() / train["label"].shape[0]
+            )
+        )
+        logger.info(
+            "percentage of actives in the external test set: {}".format(
+                test["label"].sum() / test["label"].shape[0]
+            )
+        )
+        logger.info("number of molecules in the train set: {}".format(train.shape[0]))
+        logger.info("number of molecules in the external test set: {}".format(test.shape[0]))
+
+        if i == n_splits:
+            break
 
     with open(split_path / "config.json", "w") as f:
         json.dump(config, f)
