abdcfd8d73ec5bb217c3e485af9be6bd64f1cc02
HFooladi <fooladi.hosein@gmail.com>
2024-08-09 17:04:52 +0200

    add configs for reverse molecular weight splitting

--- alinemol/splitters/splitting_configures.py
+++ alinemol/splitters/splitting_configures.py
@@ -4,6 +4,8 @@ ScaffoldSplitConfig = {"make_generic": False}
 
 MolecularWeightSplitConfig = {"generalize_to_larger": True}
 
+MolecularWeightReverseSplitConfig = {"generalize_to_larger": False}
+
 KMeansSplitConfig = {"n_clusters": 10, "metric": "euclidean"}
 
 PerimeterSplitConfig = {"n_clusters": 25, "metric": "euclidean"}
