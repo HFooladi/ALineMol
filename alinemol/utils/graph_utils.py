863515a4e3d8b5cd85c091fa16835a8f72171302
HFooladi <fooladi.hosein@gmail.com>
2024-09-15 17:35:23 +0200

    add TMD distance to graph utils

--- alinemol/utils/graph_utils.py
+++ alinemol/utils/graph_utils.py
@@ -1,15 +1,18 @@
+import copy
+from typing import Dict, Tuple
+
+import numpy as np
+import ot
 import torch
+from dgllife.data import UnlabeledSMILES
+from dgllife.utils import MolToBigraph
 from torch_geometric.data import Data
 from torch_geometric.utils import from_dgl
 
-from dgllife.data import UnlabeledSMILES
-from dgllife.utils import MolToBigraph
 
-from typing import Dict, Tuple
-
-
-
-def choose_featurizer(model:str, atom_featurizer_type:str = "canonical", bond_featurizer_type:str = "canonical") -> Tuple:
+def choose_featurizer(
+    model: str, atom_featurizer_type: str = "canonical", bond_featurizer_type: str = "canonical"
+) -> Tuple:
     """Initialize node/edge featurizer
 
     Args:
@@ -17,7 +20,7 @@ def choose_featurizer(model:str, atom_featurizer_type:str = "canonical", bond_fe
 
     Returns:
         dict: Settings with featurizers updated
-    
+
     Raises:
         ValueError: If the node_featurizer_type is not in ['canonical', 'attentivefp']
 
@@ -56,8 +59,7 @@ def choose_featurizer(model:str, atom_featurizer_type:str = "canonical", bond_fe
         node_featurizer = AttentiveFPAtomFeaturizer()
     else:
         return ValueError(
-            "Expect node_featurizer to be in ['canonical', 'attentivefp'], " "got {}".format(
-                atom_featurizer_type)
+            "Expect node_featurizer to be in ['canonical', 'attentivefp'], " "got {}".format(atom_featurizer_type)
         )
 
     if model in ["Weave", "MPNN", "AttentiveFP"]:
@@ -75,7 +77,9 @@ def choose_featurizer(model:str, atom_featurizer_type:str = "canonical", bond_fe
     return node_featurizer, edge_featurizer
 
 
-def create_dgl_graphs(smiles: list[str], node_featurizer=None, edge_featurizer=None, add_self_loop=True) -> UnlabeledSMILES:
+def create_dgl_graphs(
+    smiles: list[str], node_featurizer=None, edge_featurizer=None, add_self_loop=True
+) -> UnlabeledSMILES:
     """Create DGL graph from SMILES string
 
     Args:
@@ -83,17 +87,18 @@ def create_dgl_graphs(smiles: list[str], node_featurizer=None, edge_featurizer=N
         node_featurizer (Callable): Node featurizer
         edge_featurizer (Callable): Edge featurizer
         add_self_loop (bool): Whether to add self loop
-    
+
     Returns:
         UnlabeledSMILES: UnlabeledSMILES object
-    
+
     Note:
         UnlabeledSMILES contains the SMILES strings and the corresponding DGL graph
     """
-    mol_to_g = MolToBigraph(add_self_loop=add_self_loop, node_featurizer=node_featurizer, edge_featurizer=edge_featurizer)
+    mol_to_g = MolToBigraph(
+        add_self_loop=add_self_loop, node_featurizer=node_featurizer, edge_featurizer=edge_featurizer
+    )
     dataset = UnlabeledSMILES(smiles, mol_to_graph=mol_to_g)
     return dataset
-
 
 
 def convert_dgl_pyg(dgl_graph) -> Data:
@@ -101,28 +106,200 @@ def convert_dgl_pyg(dgl_graph) -> Data:
 
     Args:
         dgl_graph (DGLGraph): DGL graph object
-    
+
     Returns:
         PyG Data: PyG Data object
     """
     data = from_dgl(dgl_graph)
-    if "e" in data:
+    if "e" in data and "h" in data and "edge_index" in data:
         return Data(x=data["h"], edge_index=data.edge_index, edge_attr=data["e"])
-    else:
+    elif "h" in data and "edge_index" in data:
         return Data(x=data["h"], edge_index=data.edge_index)
-def create_pyg_graphs(smiles: str, model: str, add_self_loop=True):
+    elif "edge_index" in data:
+        return Data(edge_index=data.edge_index)
+
+
+def create_pyg_graphs(
+    smiles: list[str],
+    model: str,
+    atom_featurizer_type: str = "canonical",
+    bond_featurizer_type: str = "canonical",
+    add_self_loop=True,
+) -> list[Data]:
     """
-    Create PyG graph from SMILES string
+    Create PyG graph from SMILES string.
 
     Args:
-        smiles (str): SMILES string
+        smiles (list[str]): List of SMILES strings
         model (str): Model name
         add_self_loop (bool): Whether to add self loop
-    
+
     Returns:
         PyG Data: PyG Data object
     """
-    node_featurizer, edge_featurizer = choose_featurizer(model)
+    node_featurizer, edge_featurizer = choose_featurizer(model, atom_featurizer_type, bond_featurizer_type)
     dgl_graphs = create_dgl_graphs(smiles, node_featurizer, edge_featurizer, add_self_loop=add_self_loop)
     pyg_graphs = [convert_dgl_pyg(dgl_graph[1]) for dgl_graph in dgl_graphs]
     return pyg_graphs
+
+
+def get_neighbors(g: Data) -> Dict:
+    """
+    get neighbor indexes for each node
+
+    Args:
+        g : input torch_geometric graph
+
+    Returns:
+        adj: a dictionary that store the neighbor indexes
+
+    """
+    adj = {}
+    for i in range(len(g.edge_index[0])):
+        node1 = g.edge_index[0][i].item()
+        node2 = g.edge_index[1][i].item()
+        if node1 in adj.keys():
+            adj[node1].append(node2)
+        else:
+            adj[node1] = [node2]
+    return adj
+
+
+# Tree Mover's Distance solver. Credit goes to the original authors.
+# Author: Ching-Yao Chuang <cychuang@mit.edu>
+# License: MIT License
+# URL: https://github.com/chingyaoc/TMD
+def TMD(g1: Data, g2: Data, w, L: int = 4) -> float:
+    """
+    return the Tree Mover’s Distance (TMD) between g1 and g2
+
+    Args:
+        g1, g2 : two torch_geometric graphs
+        w : weighting constant for each depth
+            if it is a list, then w[l] is the weight for depth-(l+1) tree
+            if it is a constant, then every layer shares the same weight
+        L    : Depth of computation trees for calculating TMD
+
+    Returns:
+        wass : The TMD between g1 and g2
+
+    Reference:
+        Chuang et al., Tree Mover’s Distance: Bridging Graph Metrics and
+        Stability of Graph Neural Networks, NeurIPS 2022
+    """
+
+    if isinstance(w, list):
+        assert len(w) == L - 1
+    else:
+        w = [w] * (L - 1)
+
+    # get attributes
+    n1, n2 = len(g1.x), len(g2.x)
+    feat1, feat2 = g1.x, g2.x
+    adj1 = get_neighbors(g1)
+    adj2 = get_neighbors(g2)
+
+    blank = np.zeros(len(feat1[0]))
+    D = np.zeros((n1, n2))
+
+    # level 1 (pair wise distance)
+    M = np.zeros((n1 + 1, n2 + 1))
+    for i in range(n1):
+        for j in range(n2):
+            D[i, j] = torch.norm(feat1[i] - feat2[j])
+            M[i, j] = D[i, j]
+    # distance w.r.t. blank node
+    M[:n1, n2] = torch.norm(feat1, dim=1)
+    M[n1, :n2] = torch.norm(feat2, dim=1)
+
+    # level l (tree OT)
+    for l in range(L - 1):
+        M1 = copy.deepcopy(M)
+        M = np.zeros((n1 + 1, n2 + 1))
+
+        # calculate pairwise cost between tree i and tree j
+        for i in range(n1):
+            for j in range(n2):
+                try:
+                    degree_i = len(adj1[i])
+                except:
+                    degree_i = 0
+                try:
+                    degree_j = len(adj2[j])
+                except:
+                    degree_j = 0
+
+                if degree_i == 0 and degree_j == 0:
+                    M[i, j] = D[i, j]
+                # if degree of node is zero, calculate TD w.r.t. blank node
+                elif degree_i == 0:
+                    wass = 0.0
+                    for jj in range(degree_j):
+                        wass += M1[n1, adj2[j][jj]]
+                    M[i, j] = D[i, j] + w[l] * wass
+                elif degree_j == 0:
+                    wass = 0.0
+                    for ii in range(degree_i):
+                        wass += M1[adj1[i][ii], n2]
+                    M[i, j] = D[i, j] + w[l] * wass
+                # otherwise, calculate the tree distance
+                else:
+                    max_degree = max(degree_i, degree_j)
+                    if degree_i < max_degree:
+                        cost = np.zeros((degree_i + 1, degree_j))
+                        cost[degree_i] = M1[n1, adj2[j]]
+                        dist_1, dist_2 = np.ones(degree_i + 1), np.ones(degree_j)
+                        dist_1[degree_i] = max_degree - float(degree_i)
+                    else:
+                        cost = np.zeros((degree_i, degree_j + 1))
+                        cost[:, degree_j] = M1[adj1[i], n2]
+                        dist_1, dist_2 = np.ones(degree_i), np.ones(degree_j + 1)
+                        dist_2[degree_j] = max_degree - float(degree_j)
+                    for ii in range(degree_i):
+                        for jj in range(degree_j):
+                            cost[ii, jj] = M1[adj1[i][ii], adj2[j][jj]]
+                    wass = ot.emd2(dist_1, dist_2, cost)
+
+                    # summarize TMD at level l
+                    M[i, j] = D[i, j] + w[l] * wass
+
+        # fill in dist w.r.t. blank node
+        for i in range(n1):
+            try:
+                degree_i = len(adj1[i])
+            except:
+                degree_i = 0
+
+            if degree_i == 0:
+                M[i, n2] = torch.norm(feat1[i])
+            else:
+                wass = 0.0
+                for ii in range(degree_i):
+                    wass += M1[adj1[i][ii], n2]
+                M[i, n2] = torch.norm(feat1[i]) + w[l] * wass
+
+        for j in range(n2):
+            try:
+                degree_j = len(adj2[j])
+            except:
+                degree_j = 0
+            if degree_j == 0:
+                M[n1, j] = torch.norm(feat2[j])
+            else:
+                wass = 0.0
+                for jj in range(degree_j):
+                    wass += M1[n1, adj2[j][jj]]
+                M[n1, j] = torch.norm(feat2[j]) + w[l] * wass
+
+    # final OT cost
+    max_n = max(n1, n2)
+    dist_1, dist_2 = np.ones(n1 + 1), np.ones(n2 + 1)
+    if n1 < max_n:
+        dist_1[n1] = max_n - float(n1)
+        dist_2[n2] = 0.0
+    else:
+        dist_1[n1] = 0.0
+        dist_2[n2] = max_n - float(n2)
+
+    wass = ot.emd2(dist_1, dist_2, M)
+    return wass
--- tests/utils/test_graph_utils.py
+++ tests/utils/test_graph_utils.py
@@ -1,7 +1,23 @@
 # We will test all the functions available in alinemol/utils/graph_utils.py
+import numpy as np
+import pytest
+from alinemol.utils.graph_utils import (
+    choose_featurizer,
+    create_dgl_graphs,
+    convert_dgl_pyg,
+    create_pyg_graphs,
+    get_neighbors,
+    TMD,
+)
+from dgllife.utils import (
+    CanonicalAtomFeaturizer,
+    CanonicalBondFeaturizer,
+    AttentiveFPAtomFeaturizer,
+    AttentiveFPBondFeaturizer,
+    PretrainAtomFeaturizer,
+    PretrainBondFeaturizer,
+)
 
-from alinemol.utils.graph_utils import choose_featurizer
-from dgllife.utils import CanonicalAtomFeaturizer, CanonicalBondFeaturizer, AttentiveFPAtomFeaturizer, AttentiveFPBondFeaturizer, PretrainAtomFeaturizer, PretrainBondFeaturizer
 
 # Test 1: test_choose_featurizer
 def test_choose_featurizer_1():
@@ -22,6 +38,7 @@ def test_choose_featurizer_2():
     assert result[0].__class__ == CanonicalAtomFeaturizer
     assert result[1].__class__ == CanonicalBondFeaturizer
 
+
 # Test 3: test_choose_featurizer
 def test_choose_featurizer_3():
     model = "AttentiveFP"
@@ -30,6 +47,7 @@ def test_choose_featurizer_3():
     result = choose_featurizer(model, atom_featurizer_type, bond_featurizer_type)
     assert result[0].__class__ == AttentiveFPAtomFeaturizer
     assert result[1].__class__ == AttentiveFPBondFeaturizer
+
 
 # Test 4: test_choose_featurizer
 def test_choose_featurizer_4():
@@ -40,3 +58,175 @@ def test_choose_featurizer_4():
     assert result[0].__class__ == PretrainAtomFeaturizer
     assert result[1].__class__ == PretrainBondFeaturizer
 
+
+# Test 5: test_create_dgl_graphs
+def test_create_dgl_graphs_1():
+    smiles = ["C", "CC", "CCC"]
+    node_featurizer = CanonicalAtomFeaturizer()
+    edge_featurizer = CanonicalBondFeaturizer(self_loop=True)
+    result = create_dgl_graphs(smiles, node_featurizer, edge_featurizer)
+    assert len(result) == 3
+
+
+# Test 6: test_create_dgl_graphs
+def test_create_dgl_graphs_2():
+    smiles = ["C", "CC", "CCC"]
+    node_featurizer = AttentiveFPAtomFeaturizer()
+    edge_featurizer = AttentiveFPBondFeaturizer(self_loop=True)
+    result = create_dgl_graphs(smiles, node_featurizer, edge_featurizer)
+    assert len(result) == 3
+
+
+# Test 7: test_create_dgl_graphs
+def test_create_dgl_graphs_3():
+    smiles = ["C", "CC", "CCC"]
+    node_featurizer = PretrainAtomFeaturizer()
+    edge_featurizer = PretrainBondFeaturizer()
+    result = create_dgl_graphs(smiles, node_featurizer, edge_featurizer)
+    assert len(result) == 3
+
+
+# Test 8: test_convert_dgl_pyg
+def test_convert_dgl_pyg_1():
+    smiles = ["C", "CC", "CCC"]
+    node_featurizer = CanonicalAtomFeaturizer()
+    edge_featurizer = CanonicalBondFeaturizer(self_loop=True)
+    dgl_graphs = create_dgl_graphs(smiles, node_featurizer, edge_featurizer)
+    for dgl_graph in dgl_graphs:
+        pyg_graph = convert_dgl_pyg(dgl_graph[1])
+        assert pyg_graph.__class__.__name__ == "Data"
+
+
+# Test 9: test_convert_dgl_pyg
+def test_convert_dgl_pyg_2():
+    smiles = ["C", "CC", "CCC"]
+    node_featurizer = AttentiveFPAtomFeaturizer()
+    edge_featurizer = AttentiveFPBondFeaturizer(self_loop=True)
+    dgl_graphs = create_dgl_graphs(smiles, node_featurizer, edge_featurizer)
+    for dgl_graph in dgl_graphs:
+        pyg_graph = convert_dgl_pyg(dgl_graph[1])
+        assert pyg_graph.__class__.__name__ == "Data"
+
+
+# Test 10: test_convert_dgl_pyg
+def test_convert_dgl_pyg_3():
+    smiles = ["C", "CC", "CCC"]
+    node_featurizer = PretrainAtomFeaturizer()
+    edge_featurizer = PretrainBondFeaturizer()
+    dgl_graphs = create_dgl_graphs(smiles, node_featurizer, edge_featurizer)
+    for dgl_graph in dgl_graphs:
+        pyg_graph = convert_dgl_pyg(dgl_graph[1])
+        assert pyg_graph.__class__.__name__ == "Data"
+
+
+# Test 11: test_create_pyg_graphs
+def test_create_pyg_graphs_1():
+    smiles = ["C", "CC", "CCC"]
+    model = "GCN"
+    result = create_pyg_graphs(smiles, model)
+    assert len(result) == 3
+    assert result[0].__class__.__name__ == "Data"
+    assert result[0].x.shape[1] == 74
+
+
+# Test 12: test_create_pyg_graphs
+def test_create_pyg_graphs_2():
+    smiles = ["C", "CC", "CCC"]
+    model = "Weave"
+    result = create_pyg_graphs(smiles, model)
+    assert len(result) == 3
+    assert result[0].__class__.__name__ == "Data"
+    assert result[0].x.shape[1] == 74
+    assert result[0].edge_attr.shape[1] == 13
+
+
+# Test 13: test_create_pyg_graphs
+def test_create_pyg_graphs_3():
+    smiles = ["C", "CC", "CCC"]
+    model = "AttentiveFP"
+    result = create_pyg_graphs(smiles, model, atom_featurizer_type="attentivefp", bond_featurizer_type="attentivefp")
+    assert len(result) == 3
+    assert result[0].__class__.__name__ == "Data"
+    assert result[0].x.shape[1] == 39
+    assert result[0].edge_attr.shape[1] == 11
+
+
+# Test 14: test_create_pyg_graphs
+def test_create_pyg_graphs_4():
+    smiles = ["C", "CC", "CCC"]
+    model = "gin_supervised_contextpred"
+    result = create_pyg_graphs(smiles, model)
+    assert len(result) == 3
+    assert result[0].__class__.__name__ == "Data"
+
+
+# Test 15: test_get_neighbors
+def test_get_neighbors_1():
+    smiles = ["C", "CC", "CCC"]
+    model = "GCN"
+    pyg_graphs = create_pyg_graphs(smiles, model)
+    for pyg_graph in pyg_graphs:
+        neighbors = get_neighbors(pyg_graph)
+        assert neighbors.__class__ == dict
+
+
+# Test 16: test_get_neighbors
+def test_get_neighbors_2():
+    smiles = ["C", "CC", "CCC"]
+    model = "Weave"
+    pyg_graphs = create_pyg_graphs(smiles, model)
+    for pyg_graph in pyg_graphs:
+        neighbors = get_neighbors(pyg_graph)
+        assert neighbors.__class__ == dict
+
+
+# Test 17: test_TMD
+def test_TMD_1():
+    smiles = ["C", "CC", "CCC"]
+    model = "GCN"
+    pyg_graphs = create_pyg_graphs(smiles, model)
+    distance = np.zeros((len(pyg_graphs), len(pyg_graphs)))
+    for i, src_pyg_graph in enumerate(pyg_graphs):
+        for j, tgt_pyg_graph in enumerate(pyg_graphs):
+            tmd = TMD(src_pyg_graph, tgt_pyg_graph, w=1.0)
+            distance[i, j] = tmd
+    assert distance.shape == (3, 3)
+    assert distance[0, 0] == 0
+    assert distance[1, 1] == 0
+    assert distance[2, 2] == 0
+    assert distance[0, 1] > 0
+    assert distance[0, 2] > 0
+    assert distance[1, 2] > 0
+
+
+# Test 18: test_TMD
+def test_TMD_2():
+    smiles = ["C", "CC", "CCC"]
+    model = "AttentiveFP"
+    pyg_graphs = create_pyg_graphs(
+        smiles, model, atom_featurizer_type="attentivefp", bond_featurizer_type="attentivefp"
+    )
+    distance = np.zeros((len(pyg_graphs), len(pyg_graphs)))
+    for i, src_pyg_graph in enumerate(pyg_graphs):
+        for j, tgt_pyg_graph in enumerate(pyg_graphs):
+            tmd = TMD(src_pyg_graph, tgt_pyg_graph, w=1.0)
+            distance[i, j] = tmd
+    assert distance.shape == (3, 3)
+    assert distance[0, 0] == 0
+    assert distance[1, 1] == 0
+    assert distance[2, 2] == 0
+    assert distance[0, 1] > 0
+    assert distance[0, 2] > 0
+    assert distance[1, 2] > 0
+
+
+# Test 19: test_TMD
+def test_TMD_3():
+    smiles = ["C", "CC", "CCC"]
+    model = "gin_supervised_contextpred"
+    pyg_graphs = create_pyg_graphs(smiles, model)
+    distance = np.zeros((len(pyg_graphs), len(pyg_graphs)))
+    for i, src_pyg_graph in enumerate(pyg_graphs):
+        for j, tgt_pyg_graph in enumerate(pyg_graphs):
+            with pytest.raises(TypeError):
+                tmd = TMD(src_pyg_graph, tgt_pyg_graph, w=1.0)
