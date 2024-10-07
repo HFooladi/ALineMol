71c38b010ca66a208b9b37474b9945239bbcd3db
HFooladi <fooladi.hosein@gmail.com>
2024-10-07 12:39:40 +0200

    add config for LogP splitting

--- alinemol/splitters/splitting_configures.py
+++ alinemol/splitters/splitting_configures.py
@@ -13,7 +13,7 @@ MolecularWeightReverseSplitConfig = {"generalize_to_larger": False}
 
 KMeansSplitConfig = {"n_clusters": 10, "metric": "euclidean"}
 
-PerimeterSplitConfig = {"n_clusters": 25, "metric": "euclidean"}
+PerimeterSplitConfig = {"n_clusters": 10, "metric": "euclidean"}
 
 MaxDissimilaritySplitConfig = {"n_clusters": 10, "metric": "euclidean"}
 
