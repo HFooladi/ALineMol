8f635aa44ecc33c490e42e4fd3b9a6b77c22c3ee
HFooladi <fooladi.hosein@gmail.com>
2025-03-17 16:37:39 +0100

    move from setuptools to hatch for building

--- alinemol/__init__.py
+++ alinemol/__init__.py
@@ -1,14 +1,9 @@
 """ALineMol: Package for evaluating out-of-distribution performance for molecules."""
 
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("alinemol")
-except PackageNotFoundError:
-    # Package is not installed
-    __version__ = "unknown"
-
 # Import and expose key functionality
 from .models import *  # Import your models
 from .preprocessing import *  # Import preprocessing tools
 from .utils import *  # Import utility functions
+from ._version import __version__
+
+__all__ = ["__version__"]
